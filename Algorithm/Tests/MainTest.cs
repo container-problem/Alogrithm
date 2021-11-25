106efb0cdadb5b84ba9e8b8ada3bd318ed65f290
HaNJI <henhooky@gmail.com>
2021-11-25 15:09:29 +0500

    added xunit

---
+++ Algorithm/Tests/MainTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithm.Tests
+{
+    public class MainTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
