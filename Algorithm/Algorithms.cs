4e22124a2ad6a49d103ad7321bb25cd931b8233b
Alyona <Alyona-gil@mail.ru>
2021-11-30 11:13:29 +0500

    edit

--- Algorithm/Algorithms.cs
+++ Algorithm/Algorithms.cs
@@ -1,113 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace Algorithm
+public class Algorithms
 {
-    static class Algorithms
+    static void swap(ref int x, ref int y)
     {
-        public static void swap(ref int x, ref int y)
-        {
-            var t = x;
-            x = y;
-            y = t;
-        }
+        var t = x;
+        x = y;
+        y = t;
+    }
 
-        /// <summary>
-        /// Полный перебор всех перестановок
-        /// </summary>
-        /// <param name="count">кол-во контейнеров</param>
-        /// <param name="best">лучшее распределение предметов по контейнерам</param>
-        /// <param name="index">порядок индексов согласно исходному массиву</param>
-        /// <param name="bestIndex">порядок индексов для лучшего решения</param>
-        public static void FindBest(int t, int n, int[] masses,
-                int M, ref int count, ref List<List<int>> bestSol,
-                ref int[] indices, ref int[] indicesForBestSol)
+    static void rename(int[] indices, ref List<List<int>> sol) //О(n)
+    {
+        for (int i = 0; i < sol.Count; i++)
+            for (int j = 0; j < sol[i].Count; j++)
+                sol[i][j] = indices[sol[i][j]];
+    }
+
+    static void FindBest(int t, int n, int[] masses,
+            int M, ref int count, ref List<List<int>> bestSol,
+            ref int[] indices, ref int[] indicesForBestSol) //О(n!)
+    {
+        if (t == n - 1)
         {
-            if (t == n - 1)
+            List<List<int>> result = new List<List<int>>(FF(n, M, masses));
+            if (result.Count <= count)
             {
-                List<List<int>> result = new List<List<int>>(FF(n, M, masses));
-                if (result.Count < count)
-                {
-                    bestSol = result;
-                    count = result.Count;
-                    Array.Copy(indices, indicesForBestSol, n);
-                }
-            }
-            else
-            {
-                for (int j = t; j < n; ++j)
-                {
-                    swap(ref masses[t], ref masses[j]);
-                    swap(ref indices[t], ref indices[j]);
-                    t++;
-                    FindBest(t, n, masses, M, ref count, ref bestSol, ref indices, ref indicesForBestSol);
-                    t--;
-                    swap(ref masses[t], ref masses[j]);
-                    swap(ref indices[t], ref indices[j]);
-                }
+                bestSol = result;
+                count = result.Count;
+                Array.Copy(indices, indicesForBestSol, n);
             }
         }
-
-        public static List<List<int>> ExactAlg(int n, int M, int[] masses)
+        else
         {
-            List<List<int>> bestSol = new List<List<int>>();
-            int[] indices = new int[n];
-            int[] indicesForBestSol = new int[n];
-            for (int i = 0; i < n; i++)
-                indices[i] = i;
-            int count = n;
-
-            FindBest(0, n, masses, M, ref count, ref bestSol, ref indices, ref indicesForBestSol);
-            //Т. к. распределение по контейнерам было найдено для массива,
-            //отличного от исходного, необходимо переуказать индексы
-            for (int i = 0; i < count; i++)
-                for (int j = 0; j < bestSol[i].Count; j++)
-                    bestSol[i][j] = indicesForBestSol[bestSol[i][j]];
-
-            return bestSol;
-        }
-
-        public static List<List<int>> FFAlg(int n, int M, int[] masses)
-        {
-            return FF(n, M, masses);
-        }
-
-        public static List<List<int>> FFSAlg(int n, int M, int[] masses)
-        {
-            return FF(n, M, Sort.QuickSort(masses));
-        }
-
-        public static List<List<int>> FF(int n, int M, int[] masses)
-        {
-            List<List<int>> containers = new List<List<int>>();
-            List<int> containersMasses = new List<int>();
-            int k = 0; //кол-во использованных контейнеров
-            bool IsFind;
-            for (int i = 0; i < n; i++)
+            for (int j = t; j < n; ++j)
             {
-                IsFind = false;
-                for (int j = 0; j < k; j++)
-                {
-                    if (M - containersMasses[j] >= masses[i])
-                    {
-                        containers[j].Add(i);
-                        containersMasses[j] += masses[i];
-                        IsFind = true;
-                        break;
-                    }
-                }
-                if (!IsFind) //предмет не был положен в контейнер
-                {
-                    List<int> newContainer = new List<int>();
-                    newContainer.Add(i);
-                    containers.Add(newContainer);
-                    containersMasses.Add(masses[i]);
-                    k++;
-                }
+                swap(ref masses[t], ref masses[j]);
+                swap(ref indices[t], ref indices[j]);
+                t++;
+                FindBest(t, n, masses, M, ref count, ref bestSol, ref indices, ref indicesForBestSol);
+                t--;
+                swap(ref masses[t], ref masses[j]);
+                swap(ref indices[t], ref indices[j]);
             }
-            return containers;
         }
     }
+
+    public static List<List<int>> ExactAlg(int n, int M, int[] masses)//О(n!)
+    {
+        List<List<int>> bestSol = new List<List<int>>();
+        int[] indices = new int[n];
+        int[] indicesForBestSol = new int[n];
+        for (int i = 0; i < n; i++)
+            indices[i] = i;
+        int count = n;
+
+        FindBest(0, n, masses, M, ref count, ref bestSol, ref indices, ref indicesForBestSol);
+        rename(indicesForBestSol, ref bestSol);
+        return bestSol;
+    }
+
+    public static List<List<int>> FFAlg(int n, int M, int[] masses)//О(n^2)
+    {
+        return FF(n, M, masses);
+    }
+
+    public static List<List<int>> FFSAlg(int n, int M, int[] masses)//О(n^2)
+    {
+        List<List<int>> bestSol = new List<List<int>>();//О(n*log(n))
+        int[] indices = new int[n];
+        for (int i = 0; i < n; i++)
+            indices[i] = i;
+        bestSol = FF(n, M, Sort.QuickSort(masses, indices));
+        rename(indices, ref bestSol);
+        return bestSol;
+    }
+
+    static List<List<int>> FF(int n, int M, int[] masses)//О(n^2)
+    {
+        List<List<int>> containers = new List<List<int>>();
+        List<int> containersMasses = new List<int>();
+        int k = 0;
+        bool IsFind;
+        for (int i = 0; i < n; i++)
+        {
+            IsFind = false;
+            for (int j = 0; j < k; j++)
+            {
+                if (M - containersMasses[j] >= masses[i])
+                {
+                    containers[j].Add(i);
+                    containersMasses[j] += masses[i];
+                    IsFind = true;
+                    break;
+                }
+            }
+            if (!IsFind)
+            {
+                List<int> newContainer = new List<int>();
+                newContainer.Add(i);
+                containers.Add(newContainer);
+                containersMasses.Add(masses[i]);
+                k++;
+            }
+        }
+        return containers;
+    }
 }
+
 
--- Algorithm/Sort.cs
+++ Algorithm/Sort.cs
@@ -11,39 +11,41 @@ public class Sort
     }
 
     //метод возвращающий индекс опорного элемента
-    static int Partition(int[] array, int minIndex, int maxIndex)
+    static int Partition(int[] array, int minIndex, int maxIndex, int[] indices)
     {
         var pivot = minIndex - 1;
         for (var i = minIndex; i < maxIndex; i++)
         {
-            if (array[i] < array[maxIndex])
+            if (array[i] > array[maxIndex])
             {
                 pivot++;
                 Swap(ref array[pivot], ref array[i]);
+                Swap(ref indices[pivot], ref indices[i]);
             }
         }
 
         pivot++;
         Swap(ref array[pivot], ref array[maxIndex]);
+        Swap(ref indices[pivot], ref indices[maxIndex]);
         return pivot;
     }
 
-    static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+    static int[] QuickSort(int[] array, int minIndex, int maxIndex, int[] indices)
     {
         if (minIndex >= maxIndex)
         {
             return array;
         }
 
-        var pivotIndex = Partition(array, minIndex, maxIndex);
-        QuickSort(array, minIndex, pivotIndex - 1);
-        QuickSort(array, pivotIndex + 1, maxIndex);
+        var pivotIndex = Partition(array, minIndex, maxIndex, indices);
+        QuickSort(array, minIndex, pivotIndex - 1, indices);
+        QuickSort(array, pivotIndex + 1, maxIndex, indices);
 
         return array;
     }
 
-    public static int[] QuickSort(int[] array)
+    public static int[] QuickSort(int[] array, int[] indices)
     {
-        return QuickSort(array, 0, array.Length - 1);
+        return QuickSort(array, 0, array.Length - 1, indices);
     }
 }
